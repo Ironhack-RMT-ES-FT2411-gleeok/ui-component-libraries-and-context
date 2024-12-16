b3c61ad529f03a5873faef71c1fa3b4cb39fcf76
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2024-12-16 09:56:03 +0100

    agregados estilos con bootstrap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,27 @@
 import { Link } from 'react-router-dom'
 
-function Navbar() {
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function MyNavbar() {
   return (
-    <nav>
+    <Navbar expand="lg" className="bg-body-tertiary">
 
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contacts</Link>
-      <Link to="/about">About</Link>
-      <Link to="/book/list">Book List</Link>
-      <Link to="/book/add">Add a Book</Link>
-      <button>☀️/🌑</button>
+      <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/contact">Contacts</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/book/list">Book List</Nav.Link>
+          <Nav.Link as={Link} to="/book/add">Add a Book</Nav.Link>
+          <button>☀️/🌑</button>
+        </Nav>
+      </Navbar.Collapse>
 
-    </nav>
+    </Navbar>
   )
 }
 
-export default Navbar
+export default MyNavbar
--- src/pages/books/BookList.jsx
+++ src/pages/books/BookList.jsx
@@ -4,6 +4,9 @@ import img1 from "../../assets/img1.jpg"
 import img2 from "../../assets/img2.jpg"
 import img3 from "../../assets/img3.jpg"
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 function BookList() {
   return (
     <div>
@@ -12,26 +15,32 @@ function BookList() {
 
       <div>
 
-        <div>
-          <img src={img1} alt="cover" width={120} height={200}/>
-          <h3>Book1</h3>
-          <p>Lorem ipsum, dolor sit amet</p>
-          <Link to="/book/details/1">See Book 1</Link>
-        </div>
+        <Card style={{ width: '200px' }}>
+          <Card.Img variant="top" src={img1} alt="cover" width={120} height={200}/>
+          <Card.Body>
+            <Card.Title>Book1</Card.Title>
+            <Card.Text>Lorem ipsum, dolor sit amet</Card.Text>
+            <Link to="/book/details/1">
+              <Button>
+                See Book 1
+              </Button>
+            </Link>
+          </Card.Body>
+        </Card>
 
-        <div>
-          <img src={img2} alt="cover" width={120} height={200}/>
+        <Card>
+          <Card.Img src={img2} alt="cover" width={120} height={200}/>
           <h3>Book2</h3>
           <p>Lorem ipsum, dolor sit amet</p>
           <Link to="/book/details/2">See Book 2</Link>
-        </div>
+        </Card>
 
-        <div>
-          <img src={img3} alt="cover" width={120} height={200}/>
+        <Card>
+          <Card.Img src={img3} alt="cover" width={120} height={200}/>
           <h3>Book3</h3>
           <p>Lorem ipsum, dolor sit amet</p>
           <Link to="/book/details/3">See Book 3</Link>
-        </div>
+        </Card>
 
       </div>
 
